ecf379d991ad49dc34e4145a610b23f64426ba8e
Pierre Beaujean <pierreb24@gmail.com>
2020-05-02 15:46:26 +0200

    fix setup.py (again)

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
-from setuptools import setup
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
+# adapted over https://github.com/pypa/sampleproject/blob/master/setup.py
 
-pkgs = []
-dependency_links = []
-for pkg in parse_requirements('requirements.txt', session=False):
-    if pkg.link:
-        dependency_links.append(str(pkg.link))
-    else:
-        pkgs.append(str(pkg.req))
+from setuptools import setup, find_packages
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.readlines()
 
 setup(
     name='iso4',
-    packages=['iso4'],
-    version='0.1',
-    author='adlpr',
+    version=0.1,
+
+    # Description
     description='Implementation of ISO 4',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='adlpr',
+
+    # Classifiers
     classifiers=[
         'Environment :: Scientific',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3'
+        
+        # Specify the Python versions:
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-    package_data={'iso4': ['LTWA_20170914.json', 'LTWA_20170914.tsv', 'stopwords.txt']},
-    install_requires=pkgs,
+
+    packages=find_packages(),
+    python_requires='>=3.5',
+
+    # requirements
+    install_requires=requirements,
 )
